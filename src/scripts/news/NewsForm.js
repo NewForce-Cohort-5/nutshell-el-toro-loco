f371fc5cfae6b32034cd68c995e82a0d5e3b95ac
john gallagher <127highstreet@gmail.com>
2021-11-17 15:46:51 -0500

    saving

--- src/scripts/news/NewsForm.js
+++ src/scripts/news/NewsForm.js
@@ -1,1 +1,23 @@
-//author: Akiko Endo --purpose: to a save form
+//author: Akiko Endo --purpose: to a save form 
+const newArticleContentTarg = document.querySelector(".news-container") 
+
+export const newArticle = () => {
+    
+
+    newArticleContentTarg.innerHTML = `
+<button id="new-article" class="#" >New Article</button>
+`
+}
+
+//this is an event listner to bring up the form when a user clicks the newArticle button
+
+newArticleContentTarg.addEventListener("clicks", clickEvent => {
+
+    if (clickEvent.target.id = "new-article" ){
+        articleForm ()
+    }
+
+})
+
+export const articleForm ()
+
