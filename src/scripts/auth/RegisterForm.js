d542b4fd66c7acde24c47f9ed4c39e1fd0fd386a
Jaytwy24 <jtwy24@gmail.com>
2021-11-23 15:14:14 -0500

    refactored log in page

--- src/scripts/auth/RegisterForm.js
+++ src/scripts/auth/RegisterForm.js
@@ -48,33 +48,7 @@ eventHub.addEventListener("click", (e) => {
 
 const render = () => {
   contentTarget.innerHTML += `
-  <!-- Button trigger modal -->
-  <button type="button" class="btn btn-primary" id="taskEditModalButton" data-bs-toggle="modal" data-bs-target="#registerModal">
-   Register
-  </button>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">Edit Task</div>
-          <div class="modal-body">
-         
-               <section class="register">
-            <input id="register--email" class="formInput" type="text" placeholder=" Register with email">
-
-            <button class="formButton" data-bs-dismiss="modal" id="register--button">Register</button>
-        </section>
-      
-          
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>        
-        </div>
-      
-    </div>
-  </div>
-  </div>
-  </div>      
+   
   
  
     `;
